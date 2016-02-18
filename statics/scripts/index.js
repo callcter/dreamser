30a9a3e9034a35b1ce343d7c8e2b05ee1fd2546a
callcter <callcter@163.com>
2016-02-18 23:23:53 +0800

    new index

--- statics/scripts/index.js
+++ statics/scripts/index.js
@@ -1,10 +1,43 @@
 function onloaded(){
-    address();
-    var now = new Date();
-    date(now);
-    var year = document.getElementById("year");
-    var month = document.getElementById("month");
-    year.innerHTML = now.getFullYear();
-    month.innerHTML = now.getMonth()+1;
-    next(now);
+    carousel();
+}
+
+function carousel(){
+    var flag = 1;
+    var src;
+    carousel = document.getElementById("carousel");
+    indexs = document.getElementById("carousel_index").getElementsByTagName("li");
+    for(var j=0;j<3;j++){
+        indexs[j].index = j;
+        indexs[j].addEventListener("click",function(){
+            
+            for(var i=0;i<3;i++){
+                indexs[i].className = "";
+            }
+            
+            indexs[this.index].className = "active";
+            src = "statics/images/carousel"+(this.index+1)+".jpg";
+            carousel.setAttribute("src",src);
+            flag = this.index+1;
+        });
+    }
+    
+    var timer = setInterval(function(){
+        
+        for(var i=0;i<3;i++){
+            indexs[i].className = "";
+        }
+        
+        if(flag==4){
+            flag = 1;
+        }
+        
+        indexs[flag-1].className = "active";
+        src = "statics/images/carousel"+flag+".jpg";
+        carousel.setAttribute("src",src);
+        
+        flag++;
+        
+    },2000);
+    
 }

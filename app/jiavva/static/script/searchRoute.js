5045f678ff758912f23ebce1808f2e59bbcb3f4a
callcter <callcter@163.com>
2016-03-05 02:15:53 +0800

    jiavva

--- app/jiavva/static/script/searchRoute.js
+++ app/jiavva/static/script/searchRoute.js
@@ -50,93 +50,36 @@ var selectList = function () {
 var departureSelect = function () {
     var box = document.getElementById("departure");
     var selectBox = document.getElementById("selectBox1");
-    var selectTabs = document.getElementById("selectTab1").getElementsByTagName("li");
-    var selectList0 = document.getElementById("selectList10");
-    var selectList1 = document.getElementById("selectList11");
-    var selectList2 = document.getElementById("selectList12");
+    var popularList = document.getElementById("popularList");
     var boxS = document.getElementById("startPlaceBox");
-    var note = "";
-    var options = address;
-    var selected = box;
     data.departure = new Object();
     addEvent(boxS, "click", function () {
-        note = "";
         selectBox.style.top = this.offsetHeight + this.offsetTop + "px";
         selectBox.style.left = this.offsetLeft + "px";
         if (selectBox.style.display == "block") {
             selectBox.style.display = "none";
-            selectList0.style.display = "none";
-            selectList1.style.display = "none";
-            selectList2.style.display = "none";
+            popularList.style.display = "none";
         } else {
             selectBox.style.display = "block";
-            selectList0.style.display = "block";
+            popularList.style.display = "block";
         }
-        selectTabs[2].className = "";
-        selectTabs[0].className = "active";
     });
-    for (var i = 0; i < options.length; i++) {
-        var fLi = document.createElement("li");
-        fLi.innerHTML = options[i].text;
-        selectList0.appendChild(fLi);
-        addEvent(fLi, "click", function () {
-            note += this.innerHTML;
-            data.departure.provinceCode = options[liNum(this)].value;
-            selected.innerHTML = note;
-            //清空二级商品列表
-            removeAllChild(selectList1);
-            //作为三级商品列表向上访问时二级列表的缓存
-            var firstListArray = options[liNum(this)].children;
-            for (var j = 0; j < options[liNum(this)].children.length; j++) {
-                var sLi = document.createElement("li");
-                sLi.innerHTML = options[liNum(this)].children[j].text;
-                selectList1.appendChild(sLi);
-                addEvent(sLi, "click", function () {
-                    note += "-" + this.innerHTML;
-                    data.departure.cityCode = firstListArray[liNum(this)].value;
-                    selected.innerHTML = note;
-                    //清空三级商品列表
-                    removeAllChild(selectList2);
-                    var secondListArray = firstListArray[liNum(this)].children;
-                    for (var k = 0; k < firstListArray[liNum(this)].children.length; k++) {
-                        var tLi = document.createElement("li");
-                        tLi.innerHTML = firstListArray[liNum(this)].children[k].text;
-                        selectList2.appendChild(tLi);
-                        addEvent(tLi, "click", function () {
-                            note += "-" + this.innerHTML;
-                            data.departure.districtCode = secondListArray[liNum(this)].value;
-                            selected.innerHTML = note;
-                            selectList2.style.display = "none";
-                            selectBox.style.display = "none";
-                        });
-                        addEvent(tLi, "mouseover", function () {
-                            this.className = "activeT";
-                        });
-                        addEvent(tLi, "mouseout", function () {
-                            this.className = "";
-                        });
-                    }
-                    selectList1.style.display = "none";
-                    selectList2.style.display = "block";
-                    selectTabs[1].className = "";
-                    selectTabs[2].className = "active";
-                });
-                addEvent(sLi, "mouseover", function () {
-                    this.className = "activeT";
-                });
-                addEvent(sLi, "mouseout", function () {
-                    this.className = "";
-                });
-            }
-            selectList0.style.display = "none";
-            selectList1.style.display = "block";
-            selectTabs[0].className = "";
-            selectTabs[1].className = "active";
+    for(var l=0;l<popularAddr.length;l++){
+        var pLi = document.createElement("li");
+        pLi.innerHTML = popularAddr[l].text;
+        popularList.appendChild(pLi);
+        addEvent(pLi,"click",function(){
+            box.innerHTML = this.innerHTML;
+            data.departure.provinceCode = popularAddr[liNum(this)].v1;
+            data.departure.cityCode = popularAddr[liNum(this)].v2;
+            data.departure.districtCode = popularAddr[liNum(this)].v3;
+            selectBox.style.display = "none";
+            popularList.style.display = "none";
         });
-        addEvent(fLi, "mouseover", function () {
+        addEvent(pLi,"mouseover",function(){
             this.className = "activeT";
         });
-        addEvent(fLi, "mouseout", function () {
+        addEvent(pLi, "mouseout", function () {
             this.className = "";
         });
     }
@@ -379,62 +322,68 @@ var checkBox = function () {
     }
 }
 //循环生成路线
-var routeCreate = function () {
+var routeCreate = function (resObj) {
     var content = document.getElementById("content");
     var list = "";
-    for(var i=0;i<5;i++){
+    var listSub = "";
+    for(var i=0;i<resObj.length;i++){
+        for(var j=0;j<resObj[i]. solutions.length;j++){
+            listSub += "<li><div class='d1'><p>" +
+            resObj[i].solutions[j].serviceProvider +
+            "</p><ul class='icon'><li><img src='../static/image/iconfont-" +
+            "xingji" +
+            ".png'></li><li><img src='../static/image/iconfont-" +
+            "xingji" +
+            ".png'></li><li><img src='../static/image/iconfont-" +
+            "xingji" +
+            ".png'></li><li><img src='../static/image/iconfont-" +
+            "xingji" +
+            ".png'></li><li><img src='../static/image/iconfont-" +
+            "xingji" +
+            ".png'></li></ul></div><div class='d2'><p class='route'>从：" +
+            resObj[i].solutions[j].departure +
+            //"（<a href='#'>查看网点</a>）"+
+            "</p><p class='route'>从：" +
+            resObj[i].solutions[j].destination +
+            //"（<a href='#'>查看网点</a>）"+
+            "</p><p class='icons'>" +
+            "<span><img src='../static/image/baoxian.png'></span>" +
+            "<span><img src='../static/image/tiqu.png'></span>" +
+            "<span><img src='../static/image/songzhuang.png'>" +
+            "</span><span><img src='../static/image/ruanti.png'></span>" +
+            "<span><img src='../static/image/dingzhi.png'></span>" +
+            "<span><img src='../static/image/banshi.png'></span>" +
+            "<span><img src='../static/image/shimu.png'></span>" +
+            "</p><p class='type orange'>" +
+            "送装一体" +
+            "</p></div><div class='d3'><p class='time'>物流：" +
+            "1-3天" +
+            "</p><p class='time'>安装：" +
+            "1-2天" +
+            "</p><p class='type orange'>" +
+            "零担，普通汽运" +
+            "</p></div><div class='d4'><p>轻货：￥" +
+            "100" +
+            " /立方米</p><p>重货：￥" +
+            "120" +
+            " /立方米</p><p>配送：￥" +
+            "90" +
+            " /立方米</p><p>安装：￥" +
+            "30" +
+            " /组</p><p>保底：￥" +
+            "60" +
+            " /票</p></div><div class='d5'><div class='buIcon'><img src='../static/image/searchRoute/butie.png'></div><span> " +
+            "20%" +
+            "</span></div><div class='d6'><p>已成交<span class='redWord'>" +
+            "150" +
+            "</span>笔</p><p><a href='#'>" +
+            "33" +
+            "条评价</a></p></div></li>"
+        }
         list += "<li><ul class='listDetail'>" +
-        "<li><div class='d1'><p>" +
-        "一智通家具" +
-        "</p><ul class='icon'><li><img src='../static/image/iconfont-" +
-        "xingji" +
-        ".png'></li><li><img src='../static/image/iconfont-" +
-        "xingji" +
-        ".png'></li><li><img src='../static/image/iconfont-" +
-        "xingji" +
-        ".png'></li><li><img src='../static/image/iconfont-" +
-        "xingji" +
-        ".png'></li><li><img src='../static/image/iconfont-" +
-        "iconxingjikongxin" +
-        ".png'></li></ul></div><div class='d2'><p class='route'>从：" +
-        "佛山市-顺德区" +
-        //"（<a href='#'>查看网点</a>）"+
-        "</p><p class='route'>从：" +
-        "长沙市-岳麓区" +
-        //"（<a href='#'>查看网点</a>）"+
-        "</p><p class='icons'>" +
-        "<span><img src='../static/image/baoxian.png'></span>" +
-        "<span><img src='../static/image/tiqu.png'></span>" +
-        "<span><img src='../static/image/songzhuang.png'>" +
-        "</span><span><img src='../static/image/ruanti.png'></span>" +
-        "<span><img src='../static/image/dingzhi.png'></span>" +
-        "<span><img src='../static/image/banshi.png'></span>" +
-        "<span><img src='../static/image/shimu.png'></span>" +
-        "</p><p class='type orange'>" +
-        "送装一体" +
-        "</p></div><div class='d3'><p class='time'>物流：" +
-        "1-3天" +
-        "</p><p class='time'>安装：" +
-        "1-2天" +
-        "</p><p class='type orange'>" +
-        "零担，普通汽运" +
-        "</p></div><div class='d4'><p>轻货：￥" +
-        "100" +
-        " /立方米</p><p>重货：￥" +
-        "120" +
-        " /立方米</p><p>配送：￥" +
-        "90" +
-        " /立方米</p><p>安装：￥" +
-        "30" +
-        " /组</p><p>保底：￥" +
-        "60" +
-        " /票</p></div><div class='d5'><div class='buIcon'><img src='../static/image/searchRoute/butie.png'></div><span> " +
-        "20%" +
-        "</span></div><div class='d6'><p>已成交<span class='redWord'>" +
-        "150" +
-        "</span>笔</p><p><a href='#'>" +
-        "33" +
-        "条评价</a></p></div></li>" + "</ul><div class='listSummary'><div class='s1'><p>预估费用：<span class='redWord'>￥</span><span class='priceSum'>" +
+        
+         +
+        "</ul><div class='listSummary'><div class='s1'><p>预估费用：<span class='redWord'>￥</span><span class='priceSum'>" +
         "290" +
         "</span></p></div><div class='s2'><div class='confirmBtn'>下单</div>"+
         //"<div class='collect'><div id='collected'><div class='collectIcon'><img src='../static/image/iconfont-" +

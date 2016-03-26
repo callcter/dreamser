e224abce1530821586878d6f798e733d7a0296bf
callcter <callcter@163.com>
2016-03-26 17:45:35 +0800

    require

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,4 @@
-require.config({
-    paths: {
-        jquery: 'plugin/jquery',
-        index: 'helper/index'
-    }
-});
-
-require(['index'],function(index){
+require(['config','helper/index'],function(index){
     index.carousel();
     index.titleClick();
 });

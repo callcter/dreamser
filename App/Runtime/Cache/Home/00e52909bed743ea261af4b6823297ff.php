fdad9c88357f0bc639a43b6de8d032ce60fc861a
callcter <callcter@163.com>
2016-03-28 20:26:32 +0800

    php

--- App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
+++ App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
@@ -11,7 +11,7 @@
 
 <!-- 	<?php if(is_array($person)): foreach($person as $key=>$data): echo ($data['name']); ?>---------------<?php echo ($data['age']); ?><br><?php endforeach; endif; ?> -->
 
-<!-- 	<?php $__FOR_START_17999__=1;$__FOR_END_17999__=10;for($k=$__FOR_START_17999__;$k <= $__FOR_END_17999__;$k+=1){ echo ($k); ?><br><?php } ?> -->
+<!-- 	<?php $__FOR_START_12037__=1;$__FOR_END_12037__=10;for($k=$__FOR_START_12037__;$k <= $__FOR_END_12037__;$k+=1){ echo ($k); ?><br><?php } ?> -->
 
 <!-- 	<?php if($num > 10): ?>num大于10
 	<?php elseif($num < 10): ?>
--- admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-// 定义应用目录
-define('APP_NAME','Admin');
-define('APP_PATH','./Admin/');
-
-// 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';

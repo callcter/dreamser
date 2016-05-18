e0e347e1e7cbbd0ccbe64290246b084e788dd90f
callcter <callcter@163.com>
2016-05-18 19:48:41 +0800

    date

--- Public/js/date.js
+++ Public/js/date.js
@@ -4,9 +4,107 @@
 *功能三：天可添加标记
 *功能四：以月为单位前后翻页
 ***/
+window.onload = function(){
+  var container = document.getElementById('container');
+  var list = new Dsdate();
+  list.init(container);
+}
 
-function Date(event){
-  
+function Dsdate(){
+  this.date = new Date();
+}
+
+Dsdate.prototype = {
+  init: function(parent){
+    var box = document.createElement('div');
+    box.className = 'dsdate';
+    box.setAttribute('id','dsdate');
+    var year = document.createElement('div');
+    year.className = 'year';
+    this.yearInit(year);
+    var week = document.createElement('ul');
+    week.className = 'week';
+    this.weekInit(week);
+    var day = document.createElement('div');
+    day.className = 'day';
+    day.setAttribute('id','day');
+    this.dayInit(day);
+    var time = document.createElement('p');
+    time.className = 'time';
+    this.timeInit(time);
+    var btns = document.createElement('p');
+    btns.className = 'btns';
+    this.btnsInit(btns);
+    box.appendChild(year);
+    box.appendChild(week);
+    box.appendChild(day);
+    box.appendChild(time);
+    box.appendChild(btns);
+    parent.appendChild(box);
+  },
+  yearInit: function(parent){
+    parent.innerHTML = '<span id="preYear" class="btn"> < </span><input id="year" type="text"> 年<span id="nextYear" class="btn"> > </span><span id="preMonth" class="btn"> < </span><input id="month" type="text"> 月<span id="nextMonth" class="btn"> > </span>';
+  },
+  weekInit: function(parent){
+    parent.innerHTML = '<li>一</li><li>二</li><li>三</li><li>四</li><li>五</li><li>六</li><li>日</li>';
+  },
+  dayInit: function(parent){
+    var today = this.date.getDate();
+    var days_count = this.daysCount(this.date.getFullYear(),this.date.getMonth());//某个月的天数
+    var day_week_number = this.dayWeekNumber(this.date.getFullYear(),this.date.getMonth());//某个月第一天是周几
+    var liNumber = Math.ceil((days_count+day_week_number)/7);//某个月要分为多少行
+    var monthArray = new Array();
+    var num = 1;
+    for(var i=0;i<liNumber;i++){
+      monthArray[i] = [0,0,0,0,0,0,0];
+      for(var j=0;j<7;j++){
+        if(i==0&&j<day_week_number){
+          continue;
+        }else if(i==liNumber-1&&num>days_count){
+          continue;
+        }
+        monthArray[i][j] = num;
+        num++;
+      }
+    }
+    parent.innerHTML = "";
+    for(var i=0;i<liNumber;i++){
+      var ul = document.createElement("ul");
+      parent.appendChild(ul);
+      for(var j=0;j<7;j++){
+        var li = document.createElement("li");
+        if(monthArray[i][j]!=0){
+          li.innerHTML = monthArray[i][j];
+          li.addEventListener("click",function(){
+            var theDate = $("#year").html()+"-"+$("#month").html()+"-"+this.innerHTML;
+            var li_active = document.getElementsByClassName("active");
+            if(li_active.length!=0){
+              li_active[0].className = "full";
+            }
+            this.className = "active";
+            alert(theDate);
+          });
+        }
+        ul.appendChild(li);
+      }
+    }
+  },
+  timeInit: function(parent){
+    parent.innerHTML = '<input id="hour" type="text"><input id="minute" type="text"><input id="second" type="text">';
+  },
+  btnsInit: function(parent){
+    parent.innerHTML = '<button>关闭</button><button>今天</button><button>确定</button>';
+  },
+  //获取某个月有多少天
+  daysCount: function(year,month){
+    var d = new Date(year,month+1,0);
+    return d.getDate();
+  },
+  //获取某个月的第一天是周几
+  dayWeekNumber: function(year,month){
+    var d = new Date(year,month,1);
+    return d.getDay();
+  }
 }
 
 //年下拉列表

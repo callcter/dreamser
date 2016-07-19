6018bd95e6ffd484d7922395e13009ed56586307
刘杭 <callcter@diancangstudio.com>
2016-07-19 20:45:08 +0800

    React/recommend

--- App/Home/Controller/ReactController.class.php
+++ App/Home/Controller/ReactController.class.php
@@ -3,7 +3,13 @@ namespace Home\Controller;
 use Think\Controller;
 
 class ReactController extends Controller {
-    public function index(){
-        $this->display();
-    }
+  public function index(){
+    $this->display();
+  }
+  public function recommend(){
+    $Memory = M('memory');
+    $data=$Memory->select();
+    $res = json_encode($data);
+    echo $res;
+  }
 }

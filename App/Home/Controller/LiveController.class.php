8591c06c94083d49b4fd18fe055000c13dd1f8db
callcter <callcter@163.com>
2016-08-24 18:33:54 +0800

    some fixed

--- App/Home/Controller/LiveController.class.php
+++ App/Home/Controller/LiveController.class.php
@@ -7,6 +7,16 @@ class LiveController extends Controller {
 		$this->display();
 	}
 	public function douyu(){
-		echo('hi');	
+		$reg = '/\<head>';
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, 'http://m.douyu.com/639471');
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_NOBODY, false);
+		curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($curl);
+		preg_match_all($reg, $data, $list);
+		curl_close($curl);
+		var_dump($list);
 	}
 }
